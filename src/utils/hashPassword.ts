73688a570c7ef0d91aac7807c3b13ac2d3968413
Neljohn-Cerera <neljohncerera.github@gmail.com>
2022-08-01 13:56:49 -0700

    fix bug

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,21 +1,21 @@
-import { Request, Response } from "Express";
+import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import { logger } from "../utils";
 
-const hashPassword = async(req: Request, res: Response, password: string) => {
+const hashPassword = async (req: Request, res: Response, password: string) => {
   let hashedPassword;
   try {
     // hashed password
     const salt = await bcrypt.genSalt(10);
-     hashedPassword = await bcrypt.hash(password, salt);
+    hashedPassword = await bcrypt.hash(password, salt);
   } catch (error) {
-      logger.error(
-        `500 - Password hashing error - ${req.originalUrl} - ${req.method} - ${req.ip}`
-      );
-      return res.status(500).json({
-        error: true,
-        message: "Password hashing error",
-      });
+    logger.error(
+      `500 - Password hashing error - ${req.originalUrl} - ${req.method} - ${req.ip}`
+    );
+    return res.status(500).json({
+      error: true,
+      message: "Password hashing error",
+    });
   }
 
   return hashedPassword;
